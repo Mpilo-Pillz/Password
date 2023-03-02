3268eea215fa7b376d2089b0b72fd7b9bde092ee
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-03-02 07:45:46 +0200

    adding password criteria to stackview

--- Password/Password/PasswordStatusView.swift
+++ Password/Password/PasswordStatusView.swift
@@ -10,6 +10,13 @@ import UIKit
 class PasswordStatusView: UIView {
     let stackView = UIStackView()
     
+    let lengthCriteriaView = PasswordCriteriaView(text: "8-32 characters (no spaces)")
+    let uppercaseCriteriaView = PasswordCriteriaView(text: "uppercase letter (A-Z)")
+    let lowerCaseCriteriaView = PasswordCriteriaView(text: "lowercase (a-z)")
+    let digitCriteriaView = PasswordCriteriaView(text: "digit (0-9)")
+    let specialCharacterCriteriaView = PasswordCriteriaView(text: "special character (e.g. !@#$%^)")
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -36,9 +43,21 @@ extension PasswordStatusView {
         stackView.axis = .vertical
         stackView.spacing = 8
         stackView.backgroundColor = .purple
+        
+        lengthCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        uppercaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        lowerCaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        digitCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        specialCharacterCriteriaView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
+        stackView.addArrangedSubview(lengthCriteriaView)
+        stackView.addArrangedSubview(uppercaseCriteriaView)
+        stackView.addArrangedSubview(lowerCaseCriteriaView)
+        stackView.addArrangedSubview(digitCriteriaView)
+        stackView.addArrangedSubview(specialCharacterCriteriaView)
+        
         addSubview(stackView)
         
         NSLayoutConstraint.activate([
