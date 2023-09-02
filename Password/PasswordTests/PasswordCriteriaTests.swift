69cfe07592b15ac007b4681e766f690a5f4c5951
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-09-02 08:58:33 +0200

    testing length criteria

--- Password/PasswordTests/PasswordCriteriaTests.swift
+++ Password/PasswordTests/PasswordCriteriaTests.swift
@@ -5,4 +5,30 @@
 //  Created by Mpilo Pillz on 2023/09/02.
 //
 
-import Foundation
+import XCTest
+
+@testable import Password
+
+class PasswordLengthCriteriaTests: XCTestCase {
+    // Boundary condition 8-32
+    
+    func testShort() throws {
+        XCTAssertFalse(PasswordCriteria.lengthCriteriaMet("1234567"))
+    }
+    
+    func testLong() throws {
+        XCTAssertFalse(PasswordCriteria.lengthCriteriaMet("0123456789101112131415161718192021222324252627282930313233"))
+    }
+    
+    func testValidShort() throws {
+        XCTAssertTrue(PasswordCriteria.lengthCriteriaMet("12345679"))
+    }
+    
+    func testValidLong() throws {
+        XCTAssertTrue(PasswordCriteria.lengthCriteriaMet("012345678"))
+    }
+    
+    func testValidVeryLong() throws {
+        XCTAssertTrue(PasswordCriteria.lengthCriteriaMet("12345678901234567890123456789012"))
+    }
+}
